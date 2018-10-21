6ce6dd6fdb1a59b6a956d9fa0a6051163360e211
andrekorol <anrobits@yahoo.com.br>
2018-10-21 01:20:49 -0300

    Finish writing math_estra.py

--- univap/matheus/math_estra.py
+++ univap/matheus/math_estra.py
@@ -1,3 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
 #################################################################
 # 1- Gerar 3 arquivos de saída cada um referente a um parâmetro #
 #################################################################
@@ -107,7 +111,6 @@ fout.close()
 # Abrir os arquivos para leitura e gravação
 fin1 = open("estra_90N.txt")
 fin2 = open("estra_60_90N.txt")
-# fout = open("temps_difer.txt", 'w')
 
 ########################################################################
 # 2.1- Diferenças das temperaturas adjacentes no arquivo estra_90N.txt #
@@ -141,8 +144,66 @@ lines = fin2.readlines()
 for i in range(6, len(lines) - 1):
     current_temp = float(lines[i].split(tab)[1].rstrip())
     next_temp = float(lines[i + 1].split(tab)[1].rstrip())
-    temps_difer1.append(abs(round(next_temp - current_temp, 2)))
+    temps_difer2.append(abs(round(next_temp - current_temp, 2)))
 
 # Fecha os arquivos
 fin1.close()
 fin2.close()
+
+###############################################################
+# 3. Plotar os dois gráficos: (Dia X 90N, 60_90N) e Dia X 60N #
+###############################################################
+
+# Adiciona os valores de temperatura em estra_90N.txt à uma lista
+col1 = []
+with open("estra_90N.txt") as fin:
+    for line in fin.readlines()[6:]:
+        col1.append(float(line.split(tab)[1].rstrip()))
+
+# Adiciona os valores de temperatura em estra_60_90N.txt à uma lista
+col2 = []
+with open("estra_60_90N.txt") as fin:
+    for line in fin.readlines()[6:]:
+        col2.append(float(line.split(tab)[1].rstrip()))
+
+# Adiciona os valores de temperatura em estra_60N.txt à uma lista
+col3 = []
+with open("estra_60N.txt") as fin:
+    for line in fin.readlines()[6:]:
+        col3.append(float(line.split(tab)[1].rstrip()))
+
+# Cria arrays da biblioteca Numpy a partir das colunas de temperaturas
+arr1 = np.array(col1)
+arr2 = np.array(col2)
+arr3 = np.array(col3)
+
+# Plota (Dia X 90N, 60_90N)
+fig, ax = plt.subplots()
+ax.plot(arr1, 'y', label='90 N')
+ax.plot(arr2, 'g', label='60_90 N')
+plt.xlabel("Date")
+plt.ylabel("Temperature")
+plt.legend(loc='best')
+plt.show()
+
+# Plota Dia X 60N
+plt.figure()
+plt.plot(arr3, 'r')
+plt.xlabel("Date")
+plt.ylabel("Temperature - 60N")
+plt.show()
+
+###########################################################
+# 4. Gravar em um arquivo as informações calculadas i.e., #
+# as diferenças de temperaturas do item 2                 #
+###########################################################
+
+# Abri o arquivo para gravação
+fout = open("temps_difer.txt", 'w')
+
+# Escreve as diferenças de temperatura no arquivo
+for diff1, diff2 in zip(temps_difer1, temps_difer2):
+    fout.write(f"{diff1:8.2}{diff2:8.2}\n")
+
+# Fecha o arquivo
+fout.close()
