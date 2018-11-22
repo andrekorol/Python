73930baacf8d0ea4164e3efb1cc83fda0334cdeb
andrekorol <anrobits@yahoo.com.br>
2018-11-22 21:34:16 -0200

    Find SeriesPFSemTend using pandas DataFrames

--- univap/dani/pca_arvores/trees.py
+++ univap/dani/pca_arvores/trees.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
 
-series_df = pd.read_table('SeriesPFComTend.dat')
-print(series_df.mean())
+series_comtend_df = pd.read_table('SeriesPFComTend.dat')
+mean_radius_table = series_comtend_df.mean()
+series_semtend_df = series_comtend_df - mean_radius_table
+series_semtend_df.iloc[:,0] = series_comtend_df.iloc[:,0]
+
+series_comtend_df.to_csv('SeriesPFComTend.csv')
+mean_radius_table.to_csv('mean.csv')
+series_semtend_df.to_csv('SeriesPFSemTend.csv')
